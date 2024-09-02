0f402c7fe3ff273d5667d5da6871f0da1c5c735a
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-09-02 10:09:40 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "Vocễ sabe oq são jogos eletronicos?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "otimo!",
+                    "Q legal!"
+                    ]
+            },
+            {
+                texto: "Não, eu moro numa caverna escura e triste",
+                afirmacao: [
+                    "O jogo eletrônico, videojogo ou videogame é aquele que usa a tecnologia de computador. Ele pode ser jogado em computadores pessoais (dentre eles tablets e telefones celulares), em máquinas de fliperama ou em consoles.",
+                    "Que pena meu cria"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Você joga jogos eletronicos com frequência?",
+        alternativas: [
+            {
+                texto:"Sim, adoro jogos",
+                afirmacao: [
+                    "jogos são muito legais!",
+                    "jogos são muito bons só toma cuidado pra n viciar"
+                    ]
+            },
+            {
+                texto: "Não, n curto muito jogo",
+                afirmacao: [
+                    "então blz meu cria",
+                    "q paia po"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você acha q jogos eletronicos deviam ser considerados esportes?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "Poggers",
+                    "Daora!"
+                    ]
+            },
+            {
+                texto:"Não",
+                afirmacao: [
+                    "então blz fi",
+                    "Um missil está em direção a vc, se esconda ou corra, ele chegara em 2 minutos"
+                    ]
+            }
+            
+        ]
+    },
+];
