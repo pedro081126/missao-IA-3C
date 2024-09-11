a0f0f34d118f65b86d3c1ef95fd58364aa106c97
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-09-11 07:58:52 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Ken", "Ryo", "Akuma", "Cammi", "Chun-li", "Sophie"];
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.lenght);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes)
