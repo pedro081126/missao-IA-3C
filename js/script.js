827112f8b71023746f015861a983ee7b3c6209a9
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-09-30 09:16:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
